71966a137c6a01794539049377a5126eb05955ca
Lukas Tomek <l.tomek@kostal.com>
2026-05-29 13:20:19 +0200

    Remove core1 alocation memory.

--- components/wireguard/pico_stack.h
+++
@@ -1,5 +0,0 @@
-#include "Arduino.h"
-
-// Tells the framework to isolate an independent stack for Core 1.
-// This frees the shared memory space, expanding Core 0 back to a full 8KB.
-bool core1_separate_stack = true;
